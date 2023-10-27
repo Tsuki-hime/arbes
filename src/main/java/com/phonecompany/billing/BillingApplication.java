5ffd66ba7687e251f92a56e6cf09a6e8efeb236c
Tsuki-hime <karolina.sollova@seznam.cz>
2023-10-27 09:36:03 +0200

    Set up done

---
+++ src/main/java/com/phonecompany/billing/BillingApplication.java
@@ -0,0 +1,21 @@
+package com.phonecompany.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.math.BigDecimal;
+
+@SpringBootApplication
+public class BillingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillingApplication.class, args);
+        TelephoneBillCalculatorImpl calculator = new TelephoneBillCalculatorImpl();
+
+        String filepath = "";
+
+        BigDecimal total = calculator.calculate(filepath);
+        System.out.println("Total amount: " + total);
+    }
+
+}
